8d093a61ccbaab05cf31dffde432021d560f603d
xnacly <47723417+xNaCly@users.noreply.github.com>
2023-05-15 16:10:19 +0200

    WIP: roff parsing

--- manbib.go
+++ manbib.go
@@ -1,11 +1,24 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 	"os"
 
 	"github.com/xnacly/manbib/cli"
+	"github.com/xnacly/manbib/roff"
 )
+
+func curTest() {
+	r := roff.New()
+	err := r.NewInput("test.1.gz")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	v, _ := json.MarshalIndent(r.Start(), "", "  ")
+	fmt.Println(string(v))
+}
 
 func main() {
 	if len(os.Args) == 1 {
@@ -16,6 +29,8 @@ func main() {
 	switch cmd {
 	case "index", "i":
 		cli.Index()
+	case "dev":
+		curTest()
 	default:
 		log.Fatalf("unknown instruction: '%s'", cmd)
 	}
--- roff/constants.go
+++ roff/constants.go
@@ -1,1 +1,39 @@
 package roff
+
+type Token struct {
+	Kind    int
+	Line    int
+	Pos     int
+	Content string
+}
+
+const (
+	BOLD            = iota + 1 // B
+	BOLDITALIC                 // BI
+	BOLDROMAN                  // BR
+	EXAMPLESTART               // EX
+	EXAMPLEEND                 // EE
+	ITALIC                     // I
+	ITALICBOLD                 // IB
+	ITALICROMAN                // IR
+	INDENTPARAGRAPH            // IP
+	LEFTPARAGRAPH              // LP
+	MAILTOSTART                // MT
+	MAILTOEND                  // ME
+	CMDOPTION                  // OP
+	PARAGRAPH                  // P
+	ROMANBOLD                  // RB
+	ROMANITALIC                // RI
+	SMALL                      // SM
+	SMALLBOLD                  // SB
+	SECTIONHEADING             // SH
+	SYNOPSISSTART              // SY
+	SYNOPSISEND                // YS
+	TITLEHEADING               // TH
+	TAGGEDPARAGRAPH            // TP
+	URLSTART                   // UR
+	URLEND                     // UE
+	SPACINGLINE                // .
+	TEXT                       // anything else
+	NEWLINE                    // \n
+)
