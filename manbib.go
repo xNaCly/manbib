6d3467e61b87480d8577bdd129c8f07606f38b0b
xnacly <47723417+xNaCly@users.noreply.github.com>
2023-05-16 14:41:18 +0200

    feat: 'cleardb' cmd

--- manbib.go
+++ manbib.go
@@ -5,18 +5,27 @@ import (
 	"os"
 
 	"github.com/xnacly/manbib/cli"
+	"github.com/xnacly/manbib/database"
+	"github.com/xnacly/manbib/shared"
 )
 
 func main() {
-	cli.Check()
+	shared.Check()
+	database.DB = database.Setup()
+	defer database.DB.Conn.Close()
+
 	if len(os.Args) == 1 {
 		cli.StartWeb()
 		return
 	}
+
 	cmd := os.Args[1]
 	switch cmd {
 	case "index", "i":
 		cli.Index()
+	case "cleardb":
+		log.Println("clearing database...")
+		database.DB.ClearDatabase()
 	default:
 		log.Fatalf("unknown instruction: '%s'", cmd)
 	}
