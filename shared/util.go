7b852778ad998453836bc53563349c855a9c7253
xnacly <47723417+xNaCly@users.noreply.github.com>
2023-05-16 14:43:29 +0200

    feat(shared): util methods
    - moved util methods to shared pkg
    - add DEPENDENCIES array which are checked before executing
    - add shared.ConfigHome func, which returns the config dir of the currently logged in user as well as creates the config directory 'manbib' and the pandoc template

---
+++ shared/util.go
@@ -0,0 +1,63 @@
+package shared
+
+import (
+	"log"
+	"os"
+	"os/exec"
+	"path"
+	"runtime"
+)
+
+const HTML_TEMPLATE = `<nav id="$idprefix$TOC" role="doc-toc">
+$if(toc-title)$
+<h2 id="$idprefix$toc-title">$toc-title$</h2>
+$endif$
+$table-of-contents$
+</nav>
+$body$
+`
+
+var DEPENDENCIES = []string{
+	"pandoc",
+	"zcat",
+}
+
+func Check() {
+	if runtime.GOOS != "linux" {
+		log.Fatalln("manbib only supports linux")
+	}
+	for _, d := range DEPENDENCIES {
+		_, err := exec.LookPath(d)
+		if err != nil {
+			log.Fatalf("'%s' executable not found, please check its install.", d)
+		}
+	}
+}
+
+// returns the manbib config home, creates the folder if missing, creates pandoc template file
+func ConfigHome() string {
+	// ignoring this is safe, $HOME can't possibly be undefined
+	v, _ := os.UserConfigDir()
+
+	p := path.Join(v, "manbib")
+
+	_, err := os.Stat(p)
+	if err != nil {
+		err = os.Mkdir(p, os.ModePerm)
+		if err != nil {
+			log.Fatalln("couldn't create manbib config home:", err)
+		}
+	}
+
+	tPath := path.Join(p, "template.html5")
+	_, err = os.Stat(tPath)
+
+	if err != nil {
+		err := os.WriteFile(tPath, []byte(HTML_TEMPLATE), os.ModePerm)
+		if err != nil {
+			log.Fatalln("couldn't create html5 pandoc template:", err)
+		}
+	}
+
+	return p
+}
