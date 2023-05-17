56e5e59372da8e8c1488b831f0845cb765e98499
xnacly <47723417+xNaCly@users.noreply.github.com>
2023-05-17 11:56:39 +0200

    feat(cli): start webserver

--- cli/start.go
+++ cli/start.go
@@ -1,7 +1,11 @@
 package cli
 
-import "log"
+import (
+	"github.com/xnacly/manbib/web"
+	"log"
+)
 
 func StartWeb() {
-	log.Println("starting web interface")
+	log.Println("starting web interface: http://localhost:10997")
+	web.Run()
 }
