0ef686dcc25a8daee9f25f58ba35d6c3cd9dfe0a
xnacly <47723417+xNaCly@users.noreply.github.com>
2023-05-19 12:35:51 +0200

    refactor(cli): inline indexer.Index

--- cli/index.go
+++ cli/index.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 	"log"
 	"path"
+	"strings"
 	"time"
 
+	"github.com/xnacly/manbib/database"
 	"github.com/xnacly/manbib/indexer"
 	"github.com/xnacly/manbib/shared"
 )
@@ -14,16 +16,24 @@ func Index() {
 	log.Println("starting index creation, this may take a while")
 	p := indexer.Lookup()
 	l := len(p)
+	r := make([]shared.Page, 0)
 	log.Printf("found %d man pages, starting indexing", l)
 	fmt.Println()
-	templatePath := path.Join(shared.ConfigHome(), "template.html5")
 
 	// TODO: this is extremly slow, split this up into around 9k areas and spawn a goroutine for each section
 	start := time.Now()
 	for i, v := range p {
 		fmt.Print("\033[1A\033[K")
+		i += 1
 		log.Printf("processed file: [%d/%d] %.2f %%\n", i, l, (float64(i)/float64(l))*100.0)
-		indexer.Index(v, templatePath)
+
+		r = append(r, shared.Page{
+			Name:        strings.Replace(path.Base(v), ".gz", "", 1),
+			Path:        v,
+			Preview:     "",
+			LastUpdated: start,
+		})
 	}
+	database.DB.InsertPages(r)
 	log.Println("done, took: ", time.Since(start))
 }
