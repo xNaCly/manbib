7bf98677093bcfe53014a52770a7c693aae91e59
xnacly <47723417+xNaCly@users.noreply.github.com>
2023-05-19 12:37:10 +0200

    refactor(indexer): remove in-lined func

--- indexer/index.go
+++ indexer/index.go
@@ -2,39 +2,19 @@ package indexer
 
 import (
 	"io/fs"
-	"path"
 	"path/filepath"
-	"strings"
-	"time"
-
-	"github.com/xnacly/manbib/database"
-	"github.com/xnacly/manbib/shared"
 )
 
-var MAN_PATH = "/usr/share/man/"
+var MAN_PATH = "/usr/share/man"
 
 // queries the system for available man pages,
-func Lookup() (r []string) {
-	r = make([]string, 0)
+func Lookup() []string {
+	r := make([]string, 0)
 	filepath.WalkDir(MAN_PATH, func(path string, info fs.DirEntry, err error) error {
-		if !info.IsDir() {
+		if !info.IsDir() && len(path) != 0 {
 			r = append(r, path)
 		}
 		return nil
 	})
-	return
-}
-
-func Index(p string, templatePath string) {
-	// TODO: do this on the fly, if a user opens the manpage, convert it to html
-	// pandocCmd := fmt.Sprintf("man -Thtml %s", p)
-	// manPreview, _ := exec.Command("bash", "-c", pandocCmd).Output()
-
-	cmdName := strings.Replace(path.Base(p), ".gz", "", 1)
-	database.DB.InsertPage(shared.Page{
-		Name:        cmdName,
-		Path:        p,
-		Preview:     "",
-		LastUpdated: time.Now(),
-	})
+	return r
 }
